1886022eb72a853fa918768dade1663c35363db9
Sudarshan Anbazhagan <a.sudarshann@gmail.com>
2019-08-07 15:29:28 +0530

    Feature - Stock Module - Read path (#2)
    - Read the path from Vendor to import location
    - Read the inventory import schedule

---
+++ app/code/local/Mowdirect/Emailimporter/controllers/adminhtml/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+class Mowdirect_Emailimporter_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action {
+
+  /**
+  * Admin controller index action
+  *
+  * @access public
+  * @return void
+  */
+
+  public function indexAction() {
+    echo 'hello world the admin side..!!!';
+  }
+}
