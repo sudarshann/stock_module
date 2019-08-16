9551eb6fa509403f83795bf0d4936765ca1a72a3
Sudarshan Anbazhagan <a.sudarshann@gmail.com>
2019-08-16 16:09:50 +0530

    Feature(stock): improve email format

--- app/code/local/Mowdirect/Emailimporter/Model/Observer.php
+++ app/code/local/Mowdirect/Emailimporter/Model/Observer.php
@@ -22,29 +22,35 @@ class Mowdirect_Emailimporter_Model_Observer {
     }
     
     public function handleWeeklyReportCron(){
-        Mage::log('Stock importer:  weekly report cron started ');
+        Mage::log('Stock importer (info):  weekly report cron started ');
+        
         $global_email = Mage::getStoreConfig('emailimporter/vendor_email/allowed_failures_email');
         $vendors = Mage::helper('emailimporter')->get_vendors();
-        $email_output = '';
-        
+
+        $email_output = '<table>';
+        $email_output .= '<thead><th>Vendor</th><th>Hits</th><th>Misses</th></thead>';
+        $email_output .= '<tbody>';
         foreach ($vendors as $vendor) {
-            //$vendor_email = Mage::helper('emailimporter')->get_vendor_value($vendor['vendor_id'], 'email_recipient');
             $weekly_hit_count = Mage::helper('emailimporter')->get_vendor_value($vendor['vendor_id'], 'weekly_hit_count');
             $weekly_miss_count = Mage::helper('emailimporter')->get_vendor_value($vendor['vendor_id'], 'weekly_miss_count');
-            $email_output .= '<tr><td class="action-content"> vendor : '.$vendor['vendor_name'];
-            $email_output .= ' has weekly hit count of <p class="highlighted-text">'.$weekly_hit_count.'</p>';
-            $email_output .= ' &  weekly miss count of <p class="highlighted-text">'.$weekly_miss_count.'</p></td></tr>';
             
+            $email_output .= '<tr>';
+            $email_output .= '<td class="action-content">'. $vendor['vendor_name'] . '</td>';
+            $email_output .= ' <td><p class="highlighted-text">'.$weekly_hit_count.'</p></td>';
+            $email_output .= ' <td><p class="highlighted-text">'.$weekly_miss_count.'</p></td>';
+            $email_output .= '</tr>';
         }
-        
+        $email_output .= '</tbody>';
+        $email_output = '</table>';
+
         Mage::helper('emailimporter/Email')->sendEmail(
                 'emailimporter_weekly_report_template', 
-                array('name' => 'Email importer', 'email' => $global_email), 
+                array('name' => 'The Magento Stock Bot', 'email' => $global_email), 
                 $global_email, 
-                $vendor['vendor_name'], 
-                'Weekly Report on Email Importer', 
+                "The Magento Stock Bot", 
+                'Weekly Report on Email Import for different Vendors', 
                 array('email_output' => $email_output)
             );
-        Mage::log('Stock importer:  weekly report cron ended ');
+        Mage::log('Stock importer (info):  weekly report cron ended ');
     }
 }
