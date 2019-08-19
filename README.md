10e9be60bb7f511a1937e584a531534ab027f1a8
Wayne Theisinger <wayne@mowdirect.co.uk>
2019-08-19 16:24:55 +0100

    Batch importer gmail (#5)
    * Feature - Stock Module - Read path (#2)
    
    - Read the path from Vendor to import location
    - Read the inventory import schedule
    
    * Feature (Stock) : Gmail Importer (#3)
    
    * Feature(Gmail) : Partial gmail settings, sync
    
    This is a work in progress
    
    * Feature(Stock) : improve features and download
    
    * Feature (Stock) : Completed First Sprint
    
    * Bug(Stock): remove un used code
    
    * Bug (Stock) : Fix error
    
    * Feature (Stock) :Delete more than 1 email found and added logic for invalid file
    
    * Bug(stock) : fixed bug & cron weekly report
    
    * Comment out testing code
    
    * removed unnecessary die function
    
    * Bug(Stock) : fix email recipient
    
    * Feature(stock): improve email format
    
    * Bug (Stock): Remove un used code and fix condition
    
    * Bug(Stock): strict validation
    
    * Scrutinizer Auto-Fixes (#4)
    
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # stock_module
 A magento module extending uDropship that receives stock updates by email
+
+# Requirements
+Run composer install
+Run modman
+Need Google Oauth API
+Need Google service account credentails
---
+++ app/code/local/Mowdirect/Emailimporter/controllers/IndexController.php
@@ -0,0 +1,43 @@
+<?php
+
+use function GuzzleHttp\json_encode;
+
+class Mowdirect_Emailimporter_IndexController extends Mage_Core_Controller_Front_Action {
+    
+    public function indexAction() {
+        // This section is used only to debug the cron functionality manually by url.
+        // Uncomment only when testing.
+        // var_dump(Mage::getModel('emailimporter/Observer')->handleEmailCron());
+        // die();
+    }
+    
+    public function gmailredirectAction() {
+        $gmail = Mage::helper('emailimporter/GmailConnect');
+        $gmail->set_config();
+
+        if (empty($_GET['code'])) {
+            header("Location: ".$gmail->create_auth_url());
+            die();
+        }
+
+        $gmail->authenticate(['code' => $_GET['code']]);
+        
+        //$access_token = $gmail->get_token_by_code($_GET['code']);
+        $vendor_accesstoken = json_encode($gmail->get_token());
+        $vendor_refresh_token = $gmail->get_refresh_token();
+        ?>
+        <h3>ACCESS Token</h3>
+        <p>Copy this access token and paste in the vendor->preference->Email Stock Importer (Section) -> Gmail Importer Access Token</p>
+        <p style="width: 375px;line-break: normal; border: 1px solid #000;padding: 10px; word-break: break-all">
+            <?php print_r($vendor_accesstoken); ?>
+        </p>
+        <hr>
+        <h3>Refresh Token</h3>
+        <p>Copy this access token and paste in the vendor->preference->Email Stock Importer (Section) -> Gmail Importer Refresh Token</p>
+        <p style="width: 375px;line-break: normal; border: 1px solid #000;padding: 10px; word-break: break-all">
+            <?php echo $vendor_refresh_token; ?>
+        </p>
+        <?php
+        return;
+    }
+}
